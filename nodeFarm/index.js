1377725aa58af9df24fb068ca1f95a7a555bfbb1
Shogun <1790333@edu.vaniercollege.qc.ca>
2022-10-24 17:26:26 -0400

    Server Creation

--- nodeFarm/index.js
+++ nodeFarm/index.js
@@ -1,5 +1,6 @@
 // require file system module
 const fs = require('fs');
+const http = require('http');
 // home folder started an move into txt folder to txt specify the encoding
 // blocking, synchronous way
 // const txtIn= fs.readFileSync('./txt/input.txt', 'utf-8');
@@ -9,20 +10,30 @@ const fs = require('fs');
 // console.log('File Written');
 
 // non-blocking, asynchronous way
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-    if(err) return console.log('Error!');
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//     if(err) return console.log('Error!');
 
-    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-        console.log(data2);
-        fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
-            console.log(data3);
+//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//         console.log(data2);
+//         fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+//             console.log(data3);
 
-            fs.writeFile('.txt/final.txt', `${data2}\n ${data3}`, 'utf-8', err =>{
-                console.log('Your file has been written ');
-            })
-        });
-    });
+//             fs.writeFile('.txt/final.txt', `${data2}\n ${data3}`, 'utf-8', err =>{
+//                 console.log('Your file has been written ');
+//             })
+//         });
+//     });
+// });
+
+// console.log('Will read file');
+
+/////////////////////////
+// Server creation //
+const server = http.createServer((req, res) => {
+    console.log(req);
+    res.end('Hello from the server!');
+})
+
+server.listen(8000, '127.0.0.1', ()=> {
+    console.log('Server is listening on port 8000');
 });
-
-console.log('Will read file');
-
